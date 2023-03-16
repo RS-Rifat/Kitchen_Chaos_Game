2629d1e3187738c9d9e3432557157efc8536bee3
MD Rifat Hossain Chowdhury <rifat.bauet.cse@gmail.com>
2023-03-16 19:34:06 +0600

    Project File

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -110,7 +110,7 @@ public class Player : MonoBehaviour, IKitchenObjectParent
 
             //attempt only X movement
             Vector3 moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
-            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHight, playerRadius, moveDirX, moveDistance);
+            canMove = moveDir.x != 0 && !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHight, playerRadius, moveDirX, moveDistance);
 
             if (canMove)
             {
@@ -124,7 +124,7 @@ public class Player : MonoBehaviour, IKitchenObjectParent
 
                 //attempt only X movement
                 Vector3 moveDirZ = new Vector3(0, 0, moveDir.z).normalized;
-                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHight, playerRadius, moveDirZ, moveDistance);
+                canMove = moveDir.z != 0 && !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHight, playerRadius, moveDirZ, moveDistance);
 
                 if (canMove)
                 {
